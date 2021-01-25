eb2233c83e232438ba41a2163b80153aa863739b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-25 12:00:56 +0100

    Update pdf_image_extractor.py

--- pdf_image_extractor.py
+++ pdf_image_extractor.py
@@ -9,9 +9,25 @@ while True:
     dire = input("Introducir directorio: ")
     os.chdir(dire)
     file = input("Introduce archivo PDF: ")
+    images = []
 
     pdf_file = fitz.open(file)
     print('{} Páginas'.format(len(pdf_file)))
+    for page_index in range(len(pdf_file)):
+        page = pdf_file[page_index]
+        image_list = page.getImageList()
+        if image_list:
+            print('{} imágenes encontradas en la página {}'.format(len(image_list),page_index))
+        else:
+            print('0 imágenes encontradas en la página {}'.format(page_index))
+        for image_index, img in enumerate(page.getImageList(), start=1):
+            xref = img[0]
+            base_image = pdf_file.extractImage(xref)
+            image_bytes = base_image["image"]
+            image_ext = base_image["ext"]
+            image = Image.open(io.BytesIO(image_bytes))
+            image.save(open(f"image{page_index+1}_{image_index}.{image_ext}","wb"))
+        
 
     conti = ns(input("¿Continuar(n/s)?: "))
     if conti == "n":
