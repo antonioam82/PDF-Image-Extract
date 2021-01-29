b9f9358c64f87d307d3d0c83c01f66e3d22d220c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-29 11:02:57 +0100

    Update pdf_image_extractor2.py

--- pdf_image_extractor2.py
+++ pdf_image_extractor2.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.scrolledtext as slt
 import zipfile
 import io
 import os
@@ -10,10 +11,13 @@ class app():
 
         self.root = Tk()
         self.root.title('PDF Image Extractor')
-        self.root.geometry('797x470')
+        self.root.geometry('790x470')
+        self.root.configure(bg='light slate gray')
         self.current_dir = StringVar()
         self.currentDir = Entry(self.root,width=132,textvariable=self.current_dir)
         self.currentDir.place(x=0,y=0)
+        self.display = slt.ScrolledText(self.root,width=70,height=18,bg='blue',fg='light green')
+        self.display.place(x=10,y=30)
 
         self.get_dir()
         
